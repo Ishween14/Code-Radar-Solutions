6cc11e99e7b9a4b25dfa536ffd51c3fcbc57511a
Ishween14 <kaurishween8@gmail.com>
2025-02-24 14:23:14 +0530

    Solution to Print a Square Pattern

--- Print a Square Pattern@@67877e1818d53eb724f435be/code.c
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    int rows,cols;
-    scanf("%d %d",&rows,&cols);
-    for (int i=0;i<rows;i++){
-        for (int j=0;j<cols;j++){
+    int side;
+    scanf("%d",&side);
+    for (int i=0;i<side;i++){
+        for (int j=0;j<side;j++){
             printf("*");
         }
         printf("\n");
